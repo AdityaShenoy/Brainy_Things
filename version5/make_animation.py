9e05b526ea3cd68fd3968df7a874cfa9c2152ba9
AdityaShenoy <adityashenoy49@gmail.com>
2020-04-19 21:13:37 +0530

    .

--- version5/make_animation.py
+++ version5/make_animation.py
@@ -1,18 +1,71 @@
-# Open the input keyframes.csv file
-with open('keyframes.csv') as f:
+transforms = {
+  'sx': 'scaleX({})',
+  'sy': 'scaleY({})',
+  'tx': 'translateX({})',
+  'ty': 'translateY({})',
+  'r': 'rotateZ({}deg)'
+}
 
-  for line in map(str.strip, f.readlines()[1:]):
+non_transforms = {
+  'op': 'opacity: {}'
+}
 
-    row = line.split(',')
-    print(row)
+with open('keyframes.csv') as inp:
 
-    elem = row[0]
-    print(elem)
+  with open('animation.css', 'w') as out:
 
-    row_r_striped = line[len(elem)+1:].rstrip(',').split(',')
-    print(row_r_striped)
+    for line in inp.readlines()[1:]:
 
-    row_lr_striped = row_r_striped.strip(',').split(',')
-    print(row_lr_striped)
+      line = line.strip().rstrip(',')
 
-    
+      elem = line[:line.find(',')]
+
+      line = line.lstrip(elem)
+
+      frames = line.lstrip(',')
+
+      anim_delay = len(line) - len(frames) - 1
+
+      frames = frames.split(',')
+
+      anim_durn = len(frames) - 1
+
+      out.write(f'#{elem} {{\n')
+      out.write(f'  animation-name: {elem}_animation;\n')
+      out.write(f'  animation-duration: {anim_durn}s;\n')
+      out.write(f'  animation-delay: {anim_delay}s;\n')
+      out.write('}\n\n')
+      out.write(f'@keyframes {elem}_animation {{\n')
+
+      for frame_num, frame in enumerate(frames):
+        
+        if not frame:
+          continue
+      
+        out.write(f'  {frame_num / anim_durn * 100:.2f}% {{\n')
+
+        t_ops = []
+
+        for op in frame.split(';'):
+
+          prop, val = op.split(':')
+
+          if prop in transforms:
+
+            t_ops.append((prop, val))
+
+          else:
+
+            out.write(f'    {non_transforms[prop].format(val)};\n')
+
+        if t_ops:
+
+          out.write('    transform: ')
+          out.write(" ".join(
+              [transforms[prop].format(val) for prop, val in t_ops[::-1]]
+            ) + ';\n'
+          )
+
+        out.write('  }\n')
+      
+      out.write('}\n\n')
