0e7dfbb6207ec7662be37e75439935c0933c13cd
AdityaShenoy <adityashenoy49@gmail.com>
2020-06-14 17:20:17 +0530

    .

--- google_auto_download/google_auto_download.py
+++ google_auto_download/google_auto_download.py
@@ -3,41 +3,28 @@ import time
 import shutil
 import os
 
-NUM_IMAGES = 100
-IMAGE_COORD = (1495, 182)
-SAVE_COORD = (1560, 460)
-NEXT_COORD = (1830, 180)
-LOAD_TIME = 5
-SAVE_URL = 'C:\\Users\\admin\\Desktop\\google_images'
+FOLDER_PATH = 'C:\\Users\\admin\\Desktop\\google_images'
 
-if os.path.exists(SAVE_URL):
-  shutil.rmtree(SAVE_URL)
-os.mkdir(SAVE_URL)
+if os.path.exists(FOLDER_PATH):
+  shutil.rmtree(FOLDER_PATH)
+  time.sleep(1)
+os.mkdir(FOLDER_PATH)
 
 time.sleep(5)
-
-for i in range(NUM_IMAGES):
-
-  pag.rightClick(IMAGE_COORD)
+for i in range(50):
+  pag.rightClick((1485, 250))
   time.sleep(1)
-
-  pag.click(SAVE_COORD)
-  time.sleep(LOAD_TIME)
-
+  pag.click((1515, 520))
+  time.sleep(10)
   pag.typewrite(f'{i}')
   time.sleep(1)
-
   pag.hotkey('ctrl', 'l')
   time.sleep(1)
-
-  pag.typewrite(SAVE_URL)
+  pag.typewrite(FOLDER_PATH)
   time.sleep(1)
-
   pag.hotkey('alt', 's')
   time.sleep(1)
-
   pag.press('esc')
   time.sleep(1)
-
-  pag.click(NEXT_COORD)
+  pag.click((1830, 180))
   time.sleep(1)
